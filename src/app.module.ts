e4b9cee8eaed84775a962000df8436489b5a7ce1
Hader Rincon <hader.rincon@konecta-group.com>
2024-02-25 17:28:50 -0500

    Actualizar la configuración de la conexión a MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { joiValidation } from './config/joi.validation';
       rootPath: join(__dirname, '..', 'public')
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
 
     PokemonModule,
 
